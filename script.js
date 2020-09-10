e588e980b45221481797767184dfefa27a0d3777
Kit Fuderich <kitfuderich@gmail.com>
2020-09-10 00:03:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+$("#fadeIn").click(function(){
+$(".greeting").fadeIn("slow");
+})
+
+
+
+$("#fadeOut").click(function(){
+$(".greeting").fadeOut("slow");
+})
+
+
+
+
+});
